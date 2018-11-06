d7925367826e73ed682995aafb99885079844430
grant121 <grant121@JB202-L20-W-D>
2018-11-06 14:11:34 -0500

    first two finished

---
+++ WPF_Binding/SingleObject/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleObject
+{
+    class Person : ObservableObject
+    {
+        public string Name { get; set; }
+
+        int _age;
+        public int Age {
+            get { return _age; }
+            set
+            {
+                if (_age != value)
+                {
+                    _age = value;
+                    OnPropertyChanged("Age");
+                }
+            }
+        }
+    }
+}
